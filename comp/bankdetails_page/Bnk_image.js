17c62540eee19bc7da142023bee7ae684b0a6d3a
sterdingg <129440362+sterdingg@users.noreply.github.com>
2023-04-01 14:53:01 +0530

    img tag -no git suppt

--- comp/bankdetails_page/Bnk_image.js
+++ comp/bankdetails_page/Bnk_image.js
@@ -1,7 +1,8 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 function Bnk_image(props) {
     return (
-        <Image src={props.img_ref} width={parseInt(props.img_width)} height={parseInt(props.img_height)} alt={`${props.img_alt}`} />
+        // <Image src={props.img_ref} width={parseInt(props.img_width)} height={parseInt(props.img_height)} alt={`${props.img_alt}`} />
+        <img src={props.img_ref} alt={`${props.img_alt}`} />
     );
 }
 
